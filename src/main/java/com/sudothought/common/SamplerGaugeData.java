0f763b4cdb6ef95e465ed6cb93370280d03bb655
Paul Ambrose <pambrose@mac.com>
2017-04-25 21:24:08 -0700

    Update wercker and travis builds to use mvnw

--- src/main/java/com/sudothought/common/SamplerGaugeData.java
+++
@@ -1,6 +0,0 @@
-package com.sudothought.common;
-
-@FunctionalInterface
-public interface SamplerGaugeData {
-  double value();
-}
--- src/main/java/com/sudothought/package-info.java
+++
@@ -1,4 +0,0 @@
-@VersionAnnotation(version = "1.0.0", date = "05/01/17")
-package com.sudothought;
-
-import com.sudothought.common.VersionAnnotation;
