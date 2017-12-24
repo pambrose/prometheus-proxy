7b04784dbaf5ca6c6b7afc6edf7cbc394d64d3f6
Paul Ambrose <pambrose@mac.com>
2017-12-24 08:29:43 -0800

    Kotlin conversion

--- src/main/java/io/prometheus/package-info.java
+++ src/main/java/io/prometheus/package-info.java
@@ -14,7 +14,7 @@
  *  limitations under the License.
  */
 
-@VersionAnnotation(version = "1.3.0", date = "12/20/17")
+@VersionAnnotation(version = "1.3.0", date = "12/24/17")
 package io.prometheus;
 
 import io.prometheus.common.VersionAnnotation;
