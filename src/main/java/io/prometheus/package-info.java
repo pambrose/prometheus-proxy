78752168be31f9e4aad0858bea8e020325766a0c
Paul Ambrose <pambrose@mac.com>
2017-12-29 10:31:33 -0700

    Update release date

--- src/main/java/io/prometheus/package-info.java
+++ src/main/java/io/prometheus/package-info.java
@@ -14,7 +14,7 @@
  *  limitations under the License.
  */
 
-@VersionAnnotation(version = "1.3.1", date = "12/25/17")
+@VersionAnnotation(version = "1.3.1", date = "12/29/17")
 package io.prometheus;
 
 import io.prometheus.common.VersionAnnotation;
