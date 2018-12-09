bb1972314cf3e5667196683ec29517bf9970332b
Paul Ambrose <pambrose@mac.com>
2018-12-09 09:21:30 -0800

    Create 1.3.9 branch

--- src/main/java/io/prometheus/package-info.java
+++ src/main/java/io/prometheus/package-info.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-@VersionAnnotation(version = "1.3.8", date = "12/08/18")
+@VersionAnnotation(version = "1.3.9", date = "12/08/18")
 package io.prometheus;
 
 import io.prometheus.common.VersionAnnotation;
