e8e5f4452909773e71e53d7698f4767485ebd8f6
pambrose <pambrose@mac.com>
2019-12-05 09:19:33 -0800

    Update docker scripts

--- bin/docker-agent.sh
+++ bin/docker-agent.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 docker run --rm -p 8083:8083 -p 8093:8093 \
-  -e HOSTNAME=${HOSTNAME} \
-  -e AGENT_CONFIG='https://raw.githubusercontent.com/pambrose/prometheus-proxy/master/examples/simple.conf' \
+  --env AGENT_CONFIG='https://raw.githubusercontent.com/pambrose/prometheus-proxy/master/examples/simple.conf' \
+  --env PROXY_HOSTNAME=mymachine.lan \
   pambrose/prometheus-agent:1.5.0
