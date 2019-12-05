e3b058426610b2d5d2715a86214e63eeed623e5a
pambrose <pambrose@mac.com>
2019-12-05 09:20:57 -0800

    Update docker scripts

--- bin/docker-proxy.sh
+++ bin/docker-proxy.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
 docker run --rm -p 8082:8082 -p 8092:8092 -p 50051:50051 -p 8080:8080 \
-  -e HOSTNAME=${HOSTNAME} \
-  -e PROXY_CONFIG='https://raw.githubusercontent.com/pambrose/prometheus-proxy/master/examples/simple.conf' \
-  pambrose/prometheus-proxy:1.5.1
+  --env PROXY_CONFIG='https://raw.githubusercontent.com/pambrose/prometheus-proxy/master/examples/simple.conf' \
+  pambrose/prometheus-proxy:1.5.0
