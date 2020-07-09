d9e1d07c7d94bdfde3b4bad874aba91931bd780e
pambrose <pambrose@mac.com>
2020-07-08 23:08:26 -0700

    Create 1.7.1 branch

--- src/main/java/io/prometheus/package-info.java
+++ src/main/java/io/prometheus/package-info.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-@VersionAnnotation(version = "1.7.0", date = "7/8/20")
+@VersionAnnotation(version = "1.7.1", date = "7/8/20")
 package io.prometheus;
 
 import io.prometheus.common.VersionAnnotation;
