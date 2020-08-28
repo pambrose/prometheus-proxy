32e94b296b5736a7d4a86e42837c5f8e39769766
pambrose <pambrose@mac.com>
2020-08-28 12:34:49 -0700

    Create 1.9.0 branch

--- bin/docker-agent.sh
+++ bin/docker-agent.sh
@@ -3,4 +3,4 @@
 docker run --rm -p 8083:8083 -p 8093:8093 \
   --env AGENT_CONFIG='https://raw.githubusercontent.com/pambrose/prometheus-proxy/master/examples/simple.conf' \
   --env PROXY_HOSTNAME=mymachine.lan \
-  pambrose/prometheus-agent:1.8.0
+  pambrose/prometheus-agent:'1.9.0
