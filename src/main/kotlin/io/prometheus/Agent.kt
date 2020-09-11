ad612ace0459b649ccef36402cfd0d8279748fef
pambrose <pambrose@mac.com>
2020-09-10 22:37:47 -0700

    Create 1.8.6 branch

--- src/main/kotlin/io/prometheus/Agent.kt
+++ src/main/kotlin/io/prometheus/Agent.kt
@@ -50,7 +50,7 @@ import kotlin.time.TimeSource.Monotonic
 import kotlin.time.milliseconds
 import kotlin.time.seconds
 
-@Version(version = "1.8.5", date = "9/3/20")
+@Version(version = "1.8.6", date = "9/10/20")
 class Agent(val options: AgentOptions,
             inProcessServerName: String = "",
             testMode: Boolean = false,
