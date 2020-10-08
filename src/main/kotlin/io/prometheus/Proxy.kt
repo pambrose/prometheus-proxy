58bbba8481a21e0264a1d8d6c61a5b728c49c3ea
pambrose <pambrose@mac.com>
2020-10-08 14:06:44 -0700

    Update release date

--- src/main/kotlin/io/prometheus/Proxy.kt
+++ src/main/kotlin/io/prometheus/Proxy.kt
@@ -45,7 +45,7 @@ import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import kotlin.time.milliseconds
 
-@Version(version = "1.8.6", date = "9/10/20")
+@Version(version = "1.8.6", date = "10/8/20")
 class Proxy(val options: ProxyOptions,
             proxyHttpPort: Int = options.proxyHttpPort,
             inProcessServerName: String = "",
