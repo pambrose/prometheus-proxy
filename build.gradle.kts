4efd40260bb605c8e8ab4201a495c3550b97ceaa
pambrose <pambrose@mac.com>
2026-02-06 23:22:38 -0800

    Update Kotlin and dependency versions for improved stability and performance

--- build.gradle.kts
+++ build.gradle.kts
@@ -268,3 +268,16 @@ fun Project.configureDetekt() {
     baseline = file("$projectDir/config/detekt/baseline.xml")
   }
 }
+
+fun isNonStable(version: String): Boolean {
+  val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase().contains(it) }
+  val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+  val isStable = stableKeyword || regex.matches(version)
+  return !isStable
+}
+
+tasks.withType<com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask> {
+  rejectVersionIf {
+    isNonStable(candidate.version)
+  }
+}
